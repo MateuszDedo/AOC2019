4d064320f9b6eb9e669bb9c6db54a0080b1f8f5c
Mateusz Dedo <MateuszKabura@gmail.com>
2020-01-01 23:16:36 +0100

    Refactoring for day12

--- Day12.cs
+++ Day12.cs
@@ -1,5 +1,6 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace AOC2019
 {
     public class Day12 : Solution
@@ -9,181 +10,94 @@ namespace AOC2019
             public int x;
             public int y;
             public int z;
-
             public int vX;
             public int vY;
             public int vZ;
-
-            public Moon(){}
             public Moon(int x, int y, int z)
             {
-                this.x = x;
-                this.y = y;
-                this.z = z;
+                this.x = x; this.y = y; this.z = z;
             }
-
             public void calculateNewVelocity(Moon m1)
             {
-                if (m1.x > x)
-                {
-                    vX++;
-                }
-                else if (m1.x < x)
-                {
-                    vX--;
-                }
-
-                if (m1.y > y)
-                {
-                    vY++;
-                }
-                else if (m1.y < y)
-                {
-                    vY--;
-                }
-
-                if (m1.z > z)
-                {
-                    vZ++;
-                }
-                else if (m1.z < z)
-                {
-                    vZ--;
-                }
+                if (m1.x > x) vX++; else if (m1.x < x) vX--;
+                if (m1.y > y) vY++; else if (m1.y < y) vY--;
+                if (m1.z > z) vZ++; else if (m1.z < z) vZ--;
             }
-
             public void move()
             {
-                x += vX;
-                y += vY;
-                z += vZ;
+                x += vX; y += vY; z += vZ;
             }
-
             public int getPotentialEnergy()
             {
                 return Math.Abs(x) + Math.Abs(y) + Math.Abs(z);
             }
-
             public int getKineticEnergy()
             {
                 return Math.Abs(vX) + Math.Abs(vY) + Math.Abs(vZ);
             }
         }
-
+        private void calculateVelocity(List<Moon> moons)
+        {
+            foreach (Moon m in moons)
+                foreach (Moon m2 in moons)
+                    if (m != m2) m.calculateNewVelocity(m2);
+        }
+        private void moveMoons(List<Moon> moons)
+        {
+            moons.ForEach(m => m.move());
+        }
+        private int calculateEnergy(List<Moon> moons)
+        {
+            return (from s in moons select s.getPotentialEnergy() * s.getKineticEnergy()).Sum();
+        }
+        private List<Moon> createMoons()
+        {
+            return new List<Moon>{new Moon(13, 9, 5), new Moon(8, 14, -2), new Moon(-5, 4, 11), new Moon(2, -6, 1)};
+        }
         override public void part1()
         {
-            Moon m1 = new Moon(13, 9, 5);
-            Moon m2 = new Moon(8, 14, -2);
-            Moon m3 = new Moon(-5, 4, 11);
-            Moon m4 = new Moon(2, -6, 1);
-
+            List<Moon> moons = createMoons();
             int energy = 0;
-
             for (int i = 0 ; i < 1000; i++)
             {
-
-                m1.calculateNewVelocity(m2);
-                m1.calculateNewVelocity(m3);
-                m1.calculateNewVelocity(m4);
-
-                m2.calculateNewVelocity(m1);
-                m2.calculateNewVelocity(m3);
-                m2.calculateNewVelocity(m4);
-
-                m3.calculateNewVelocity(m1);
-                m3.calculateNewVelocity(m2);
-                m3.calculateNewVelocity(m4);
-
-                m4.calculateNewVelocity(m1);
-                m4.calculateNewVelocity(m2);
-                m4.calculateNewVelocity(m3);
-
-                
-                m1.move();
-                m2.move();
-                m3.move();
-                m4.move();
-
-                energy = (m1.getPotentialEnergy() * m1.getKineticEnergy()) +
-                (m2.getPotentialEnergy() * m2.getKineticEnergy()) +
-                (m3.getPotentialEnergy() * m3.getKineticEnergy()) +
-                (m4.getPotentialEnergy() * m4.getKineticEnergy());
-
-                
+                calculateVelocity(moons);
+                moveMoons(moons);
+                energy = calculateEnergy(moons);
             }
             System.Console.WriteLine("Day 12. Solution1:" + energy.ToString());
         }
-
         override public void part2()
         {
-            Moon m1 = new Moon(13, 9, 5);
-            Moon m2 = new Moon(8, 14, -2);
-            Moon m3 = new Moon(-5, 4, 11);
-            Moon m4 = new Moon(2, -6, 1);
-
+           List<Moon> moons = createMoons();
             int period = 0;
             int xAxisPeriod = 0;
             int yAxisPeriod = 0;
             int zAxisPeriod = 0;
             while (xAxisPeriod == 0 || yAxisPeriod == 0 || zAxisPeriod == 0)
             {
-                m1.calculateNewVelocity(m2);
-                m1.calculateNewVelocity(m3);
-                m1.calculateNewVelocity(m4);
-
-                m2.calculateNewVelocity(m1);
-                m2.calculateNewVelocity(m3);
-                m2.calculateNewVelocity(m4);
-
-                m3.calculateNewVelocity(m1);
-                m3.calculateNewVelocity(m2);
-                m3.calculateNewVelocity(m4);
-
-                m4.calculateNewVelocity(m1);
-                m4.calculateNewVelocity(m2);
-                m4.calculateNewVelocity(m3);
-
-                
-                m1.move();
-                m2.move();
-                m3.move();
-                m4.move();
-
-                
-                if (xAxisPeriod == 0) period++;
-                if (m1.vX == 0 && m2.vX == 0 && m3.vX == 0 && m4.vX == 0 && m1.x == 13 && m2.x == 8 && m3.x == -5 && m4.x == 2)
+                calculateVelocity(moons);
+                moveMoons(moons);
+                period++;
+                if (moons[0].vX == 0 && moons[1].vX == 0 && moons[2].vX == 0 && moons[3].vX == 0 && 
+                    moons[0].x == 13 && moons[1].x == 8 && moons[2].x == -5 && moons[3].x == 2)
                     xAxisPeriod = period;
 
-                if (m1.vY == 0 && m2.vY == 0 && m3.vY == 0 && m4.vY == 0 && m1.y == 9 && m2.y == 14 && m3.y == 4 && m4.y == -6)
+                if (moons[0].vY == 0 && moons[1].vY == 0 && moons[2].vY == 0 && moons[3].vY == 0 && 
+                    moons[0].y == 9 && moons[1].y == 14 && moons[2].y == 4 && moons[3].y == -6)
                     yAxisPeriod = period;
 
-                if (m1.vZ == 0 && m2.vZ == 0 && m3.vZ == 0 && m4.vZ == 0 && m1.z == 5 && m2.z == -2 && m3.z == 11 && m4.z == 1)
+                if (moons[0].vZ == 0 && moons[1].vZ == 0 && moons[2].vZ == 0 && moons[3].vZ == 0 &&
+                    moons[0].z == 5 && moons[1].z == -2 && moons[2].z == 11 && moons[3].z == 1)
                     zAxisPeriod = period;
             }
             System.Console.WriteLine("Day 12. Solution2:" + getCommonResult(xAxisPeriod, yAxisPeriod, zAxisPeriod).ToString());
         }
-
         public long getCommonResult(long a, long b, long c)
         { 
             long pA = a;
             long pB = b;
             long pC = c;
-            while (a != b || b != c || a != c) 
-            {
-            
-                if (a > b) 
-                {
-                    b += pB;
-                }
-                else if (b > c)
-                {
-                    c += pC;
-                }
-                else if (c > a)
-                {
-                    a += pA;
-                }
-            }
+            while (a != b || b != c || a != c) if (a > b) b += pB; else if (b > c) c += pC; else if (c > a) a += pA;
             return a;
         }
     }
